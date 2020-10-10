eff704215d28a92bfc6adb94a2a34f734136acfb
Guillermo Frias <guillef1@gmail.com>
2020-10-10 13:50:41 +0200

    Initial Commit

---
+++ TinyNews WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinyNews WatchKit Extension
+//
+//  Created by Guillermo Frias on 10/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinyNews WatchKit Extension/TinyNewsApp.swift
@@ -0,0 +1,19 @@
+//
+//  TinyNewsApp.swift
+//  TinyNews WatchKit Extension
+//
+//  Created by Guillermo Frias on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TinyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
